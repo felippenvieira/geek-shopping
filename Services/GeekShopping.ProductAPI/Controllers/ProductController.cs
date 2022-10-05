e9657f5e212961a19ef1929e30a428bd6d2b1b8e
FENA <felippe.vieira@gft.com>
2022-10-05 10:09:48 -0300

    frombody method

--- Services/GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ Services/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -31,7 +31,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        public async Task<ActionResult<ProductVO>> Create([FromBody] ProductVO vo)
         {
             if (vo is null) return BadRequest();
             var product = await _repository.Create(vo);
@@ -40,7 +40,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPut]
-        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        public async Task<ActionResult<ProductVO>> Update([FromBody] ProductVO vo)
         {
             if (vo is null) return BadRequest();
             var product = await _repository.Update(vo);
